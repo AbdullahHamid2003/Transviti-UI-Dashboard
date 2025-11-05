bd611a613f6b10828a53126629f78b88ca158c83
AbdullahHamid2003 <abdullah.hamid955@gmail.com>
2025-11-05 13:12:51 +0500

    fonts family add

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        neuehaas: ['"Neue Haas Display"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
